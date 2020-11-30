ab1bf68200cb777e90f47883879e628021aa4e3c
Angelo Brandão <angelobms@gmail.com>
2020-11-30 20:48:48 -0300

    Added injection points with the @Autowired annotation.

--- src/main/java/com/algaworks/algafood/di/notification/NotifierEmail.java
+++ src/main/java/com/algaworks/algafood/di/notification/NotifierEmail.java
@@ -1,32 +1,16 @@
 package com.algaworks.algafood.di.notification;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.model.Client;
 
+@Component
 public class NotifierEmail implements Notifier {
-	
-	private boolean capsLock;
-	private String serverHostSMTP;
-	
-	public NotifierEmail(String serverHostSMTP) {
-		this.serverHostSMTP = serverHostSMTP;
-		System.out.println("NotifierEmail");
-	}
 
 	@Override
 	public void notify(Client client, String message) {
-		if (this.capsLock) {
-			message = message.toUpperCase();
-		}
-		
 		System.out.printf("Notifying %s through email %s using SMTP %s: %s\n", 
-				client.getName(), client.getEmail(), this.getServerHostSMTP(), message);
+				client.getName(), client.getEmail(), message);
 	}
 
-	public void setCapsLock(boolean capsLock) {
-		this.capsLock = capsLock;
-	}
-	
-	public String getServerHostSMTP() {
-		return serverHostSMTP;
-	}
 }
--- src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
+++ src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
@@ -1,17 +1,16 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.model.Client;
 import com.algaworks.algafood.di.notification.Notifier;
-
+	
+@Component
 public class ClientActivationService {
 	
+	@Autowired
 	private Notifier notifier;
-	
-	public ClientActivationService(Notifier notifier) {
-		this.notifier = notifier;
-		
-		System.out.println("ClientActivationService: " + notifier);
-	}
 
 	public void activate(Client client) {
 		client.activate();
