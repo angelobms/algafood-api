223d1ddcf1eddeef74614cf5ad8d8871c8700fd1
Angelo Brandão <angelobms@gmail.com>
2020-11-30 20:50:30 -0300

    Fixed bug in the method notify.

--- src/main/java/com/algaworks/algafood/di/notification/NotifierEmail.java
+++ src/main/java/com/algaworks/algafood/di/notification/NotifierEmail.java
@@ -9,7 +9,7 @@ public class NotifierEmail implements Notifier {
 
 	@Override
 	public void notify(Client client, String message) {
-		System.out.printf("Notifying %s through email %s using SMTP %s: %s\n", 
+		System.out.printf("Notifying %s through email %s: %s\n", 
 				client.getName(), client.getEmail(), message);
 	}
 
