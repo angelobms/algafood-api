fcbd65790ef2da5a027ac5ab5afd2000617733eb
Angelo Brandão <angelobms@gmail.com>
2021-01-24 13:32:18 -0300

    Mapped the entity id to autoincrement.

--- src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
+++ src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
@@ -1,18 +1,17 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_kitchens")
 public class Kitchen {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nm_kitchen", length = 30)
 	private String name;
 
 	public Long getId() {
