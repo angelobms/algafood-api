030130cd21a7f64d123574239b500417a58a244d
Angelo Brandão <angelobms@gmail.com>
2021-01-24 17:01:05 -0300

    Fetching an object by id from the database.

---
+++ src/main/java/com/algaworks/algafood/jpa/SearchKitchenMain.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Kitchen;
+
+public class SearchKitchenMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RegisterKitchen registerKitchen = applicationContext.getBean(RegisterKitchen.class);
+		Kitchen kitchen = registerKitchen.find(1L);
+		
+		System.out.println(kitchen.getName());
+		
+	}
+
+}
