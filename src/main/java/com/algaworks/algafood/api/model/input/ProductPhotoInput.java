1a86cf932154911bd50f16bd58a48cda69d52ab8
angelobms <angelobms@gmail.com>
2022-04-21 17:48:19 -0300

    Validating the content type of the file.

--- src/main/java/com/algaworks/algafood/api/model/input/ProductPhotoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/ProductPhotoInput.java
@@ -3,8 +3,10 @@ package com.algaworks.algafood.api.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 
 import lombok.Getter;
@@ -13,11 +15,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProductPhotoInput {
-	
+
 	@NotNull
 	@FileSize(max = "100KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile file;
-	
+
 	@NotBlank
 	private String description;
 
